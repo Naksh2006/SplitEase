79e65aa5caa38a05f37e3d4c5f23a90d01389b2d
Naksh2006 <nakshtrag5@gmail.com>
2025-12-18 17:03:56 +0530

    Add UI rendering logic

---
+++ js/domRenderer.js
@@ -0,0 +1,90 @@
+import { appState } from "./appState.js";
+
+export function getActiveProject() {
+  return appState.projects.find(p => p.id === appState.activeProjectId);
+}
+
+export function renderProjects() {
+  const box = document.getElementById("projectList");
+  box.innerHTML = "";
+
+  appState.projects.forEach(p => {
+    const span = document.createElement("span");
+    span.textContent = p.name;
+
+    if (p.id === appState.activeProjectId) {
+      span.classList.add("active-project");
+    }
+
+    span.onclick = () => {
+      appState.activeProjectId = p.id;
+      renderAll();
+    };
+
+    box.appendChild(span);
+  });
+}
+
+
+export function renderMembers(project) {
+  const list = document.getElementById("memberList");
+  const paid = document.getElementById("paidBySelect");
+  const part = document.getElementById("participantsContainer");
+
+  list.innerHTML = paid.innerHTML = part.innerHTML = "";
+
+  project.members.forEach(m => {
+    list.innerHTML += `<span>${m}</span>`;
+    paid.innerHTML += `<option>${m}</option>`;
+    part.innerHTML += `
+      <div>
+        <input type="checkbox" value="${m}" checked>
+        ${m}
+        <input type="number" id="percent-${m}" placeholder="%" style="display:none">
+      </div>`;
+  });
+}
+
+export function renderBalances(project) {
+  const box = document.getElementById("balances");
+  box.innerHTML = "";
+
+  Object.entries(project.balances).forEach(([p, b]) => {
+    box.innerHTML += `<div class="${b >= 0 ? "balance-positive" : "balance-negative"}">
+      ${p}: ${b.toFixed(2)}
+    </div>`;
+  });
+}
+
+export function renderSettlements(project) {
+  const box = document.getElementById("settlements");
+  box.innerHTML = "";
+
+  project.settlements.forEach((s, i) => {
+    box.innerHTML += `
+      <div>
+        ${s.from} pays ${s.to} ₹${s.amount}
+        <button data-idx="${i}">Done</button>
+      </div>`;
+  });
+}
+
+export function renderActivity(project) {
+  const box = document.getElementById("activityLog");
+  box.innerHTML = "";
+
+  project.activity.forEach(a => {
+    box.innerHTML += `<div>[${a.time}] ${a.text}</div>`;
+  });
+}
+
+export function renderAll() {
+  const project = getActiveProject();
+  if (!project) return;
+
+  renderProjects();
+  renderMembers(project);
+  renderBalances(project);
+  renderSettlements(project);
+  renderActivity(project);
+}
